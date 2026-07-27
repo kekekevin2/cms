8095f13eb463ce98962e70ba234a3bfcce767526
justmyrdg <justmyr10@gmail.com>
2026-07-27 13:25:23 +0800

    a

--- client/src/app/environments/environment.prod.ts
+++ client/src/app/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000/api',
-  serverUrl: 'http://localhost:3000',
+  apiUrl: 'https://cms-1-vx3i.onrender.com/api',
+  serverUrl: 'https://cms-1-vx3i.onrender.com',
   // Google reCAPTCHA - Replace with your production site key
   // Get your keys from: https://www.google.com/recaptcha/admin/create
   recaptchaSiteKey: '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI',
